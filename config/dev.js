b56fa675416e3b33ce58a7f42c5a3fc267b53cb1
Beny.K <Beku1997@gmail.com>
2022-02-10 15:30:20 +0200

    first commit

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 'dbURL': 'mongodb://localhost:27017',
+  'dbURL':'mongodb+srv://beku:123@cluster0.uwlx8.mongodb.net/user?retryWrites=true&w=majority'
+}
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL':'mongodb+srv://beku:123@cluster0.uwlx8.mongodb.net/user?retryWrites=true&w=majority'
+}
