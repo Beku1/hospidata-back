4aabbbed47f35cc5bbe0374f627a6ea8d48bdac6
Daniel Radia <danielradia21@gmail.com>
2022-02-14 17:54:27 +0200

    back oved

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,6 +1,6 @@
-const userService = require('./user.service')
-const socketService = require('../../services/socket.service')
-const logger = require('../../services/logger.service')
+const userService = require('./user.service');
+const socketService = require('../../services/socket.service');
+const logger = require('../../services/logger.service');
 
 // async function getUser(req, res) {
 //     try {
@@ -14,63 +14,73 @@ const logger = require('../../services/logger.service')
 
 async function getUser(req, res) {
     try {
-        const filterBy ={by:req.query.by, content:req.query.content}
+        const filterBy = { by: req.query.type, content: req.query.userId };
         // const user = await userService.getById(req.params.id)
-        const user = await userService.getUserByFilter(filterBy)
-        res.send(user)
+
+        const user = await userService.getUserByFilter(filterBy);
+        res.send(user);
     } catch (err) {
-        logger.error('Failed to get user', err)
-        res.status(500).send({ err: 'Failed to get user' })
+        logger.error('Failed to get user', err);
+        res.status(500).send({ err: 'Failed to get user' });
     }
 }
 
-async function getLoggedInUser(req,res){
-    try{
-       const user = req.session?.user
-       delete user.password
-       res.send(user)
-    }catch(err){
-        logger.error('Failed to get Logged in user', err)
-        res.status(500).send({ err: 'Failed to get Logged in user' })
+async function getLoggedInUser(req, res) {
+    try {
+        const loggedInUser = req.session?.user;
+        const user = await userService.getByUID(loggedInUser.UID);
+        delete user.password;
+        res.send(user);
+    } catch (err) {
+        logger.error('Failed to get Logged in user', err);
+        res.status(500).send({ err: 'Failed to get Logged in user' });
     }
 }
-
 
 async function getUsers(req, res) {
     try {
         const filterBy = {
             txt: req.query?.txt || '',
-            type:req.query?.type||''
-        }
-        const users = await userService.query(filterBy)
-        console.log(users)
-        res.send(users)
+            type: req.query?.type || '',
+        };
+        const users = await userService.query(filterBy);
+        res.send(users);
     } catch (err) {
-        logger.error('Failed to get users', err)
-        res.status(500).send({ err: 'Failed to get users' })
+        logger.error('Failed to get users', err);
+        res.status(500).send({ err: 'Failed to get users' });
     }
 }
 
 async function deleteUser(req, res) {
     try {
-        await userService.remove(req.params.id)
-        res.send({ msg: 'Deleted successfully' })
+        await userService.remove(req.params.id);
+        res.send({ msg: 'Deleted successfully' });
     } catch (err) {
-        logger.error('Failed to delete user', err)
-        res.status(500).send({ err: 'Failed to delete user' })
+        logger.error('Failed to delete user', err);
+        res.status(500).send({ err: 'Failed to delete user' });
     }
 }
 
 async function updateUser(req, res) {
     try {
-        const loggedInUser= req.session.user
-        const user = req.body
-        const savedUser = await userService.update(user)
-        if(loggedInUser._id===savedUser._id) req.session.user = savedUser
-        res.send(savedUser)
+        const loggedInUser = req.session.user;
+        const user = req.body;
+        const savedUser = await userService.update(user);
+        socketService.emitToUser({
+            type: 'user-updated',
+            data: savedUser,
+            userId: savedUser._id,
+        });
+        console.log(
+            'file: user.controller.js   line 68   savedUser',
+            savedUser
+        );
+
+        if (loggedInUser.UID === savedUser.UID) req.session.user = savedUser;
+        res.send(savedUser);
     } catch (err) {
-        logger.error('Failed to update user', err)
-        res.status(500).send({ err: 'Failed to update user' })
+        logger.error('Failed to update user', err);
+        res.status(500).send({ err: 'Failed to update user' });
     }
 }
 
@@ -79,5 +89,5 @@ module.exports = {
     getUsers,
     deleteUser,
     updateUser,
-    getLoggedInUser
-}
+    getLoggedInUser,
+};
--- api/user/user.service.js
+++ api/user/user.service.js
@@ -1,205 +1,202 @@
-const dbService = require('../../services/db.service')
-const logger = require('../../services/logger.service')
-const reviewService = require('../review/review.service')
-const ObjectId = require('mongodb').ObjectId
+const dbService = require('../../services/db.service');
+const logger = require('../../services/logger.service');
+const reviewService = require('../review/review.service');
+const ObjectId = require('mongodb').ObjectId;
 
 module.exports = {
-  query,
-  getById,
-  getByUsername,
-  remove,
-  update,
-  add,
-  getByUID,
-  getIsUserTaken,
-  getUserByFilter
-}
+    query,
+    getById,
+    getByUsername,
+    remove,
+    update,
+    add,
+    getByUID,
+    getIsUserTaken,
+    getUserByFilter,
+};
 
 async function query(filterBy = {}) {
-  const criteria = _buildCriteria(filterBy)
-  try {
-    const collection = await dbService.getCollection('user')
-    var users = await collection.find(criteria).toArray()
-    console.log('users',users)
-    users = users.map((user) => {
-      delete user.password
-      return user
-    })
-    return users
-  } catch (err) {
-    logger.error('cannot find users', err)
-    throw err
-  }
+    const criteria = _buildCriteria(filterBy);
+    try {
+        const collection = await dbService.getCollection('user');
+        var users = await collection.find(criteria).toArray();
+
+        users = users.map((user) => {
+            delete user.password;
+            return user;
+        });
+        return users;
+    } catch (err) {
+        logger.error('cannot find users', err);
+        throw err;
+    }
 }
 
-
-async function getUserByFilter({by,content}){
-    try{
-        let user
-        const collection = await dbService.getCollection('user')
-        if(by==='id') user = await collection.findOne({ _id: ObjectId(content) })
-        else user = await collection.findOne({[by]:content})
-        delete user.password
-        return user
-    }catch(err){
-
-    }
+async function getUserByFilter({ by, content }) {
+    try {
+        let user;
+        const collection = await dbService.getCollection('user');
+        if (by === 'id')
+            user = await collection.findOne({ _id: ObjectId(content) });
+        else user = await collection.findOne({ [by]: content });
+        delete user.password;
+        return user;
+    } catch (err) {}
 }
 
 async function getById(userId) {
-  try {
-    const collection = await dbService.getCollection('user')
-    const user = await collection.findOne({ _id: ObjectId(userId) })
-    delete user.password
-    return user
-  } catch (err) {
-    logger.error(`while finding user ${userId}`, err)
-    throw err
-  }
+    try {
+        const collection = await dbService.getCollection('user');
+        const user = await collection.findOne({ _id: ObjectId(userId) });
+        delete user.password;
+        return user;
+    } catch (err) {
+        logger.error(`while finding user ${userId}`, err);
+        throw err;
+    }
 }
 async function getByUsername(username) {
-  try {
-    const collection = await dbService.getCollection('user')
-    const user = await collection.findOne({ username })
-    return user
-  } catch (err) {
-    logger.error(`while finding user ${username}`, err)
-    throw err
-  }
+    try {
+        const collection = await dbService.getCollection('user');
+        const user = await collection.findOne({ username });
+        return user;
+    } catch (err) {
+        logger.error(`while finding user ${username}`, err);
+        throw err;
+    }
 }
 
 async function getByUID(UID) {
-  try {
-    const collection = await dbService.getCollection('user')
-    const user = await collection.findOne({ UID })
-    return user
-  } catch (err) {
-    logger.error(`while finding user ${username}`, err)
-    throw err
-  }
+    try {
+        const collection = await dbService.getCollection('user');
+        const user = await collection.findOne({ UID });
+        return user;
+    } catch (err) {
+        logger.error(`while finding user ${username}`, err);
+        throw err;
+    }
 }
 
 async function getIsUserTaken(username, UID) {
-  let isTaken = false
-  let userUsername
-  let userUID
-  const collection = await dbService.getCollection('user')
-  try {
-    userUID = await collection.findOne({ UID })
-    if (userUID) isTaken = true
-  } finally {
-    if (!isTaken) userUsername = await collection.findOne({ username })
-    if (userUsername) isTaken = true
-    return isTaken
-  }
-}
-
-
-async function getDoctors(filterBy={}){
-    const criteria = _buildCriteria(filterBy)
+    let isTaken = false;
+    let userUsername;
+    let userUID;
+    const collection = await dbService.getCollection('user');
     try {
-      const collection = await dbService.getCollection('user')
-      var users = await collection.find(criteria).toArray()
-      users = users.map((user) => {
-        delete user.password
-       
-        return user.type==='doctor'
-      })
-      return users
-    } catch (err) {
-      logger.error('cannot find users', err)
-      throw err
+        userUID = await collection.findOne({ UID });
+        if (userUID) isTaken = true;
+    } finally {
+        if (!isTaken) userUsername = await collection.findOne({ username });
+        if (userUsername) isTaken = true;
+        return isTaken;
     }
 }
 
-
-async function getPatients(filterBy={}){
-    const criteria = _buildCriteria(filterBy)
+async function getDoctors(filterBy = {}) {
+    const criteria = _buildCriteria(filterBy);
     try {
-      const collection = await dbService.getCollection('user')
-      var users = await collection.find(criteria).toArray()
-      users = users.map((user) => {
-        delete user.password
-       
-        return user.type==='doctor'
-      })
-      return users
+        const collection = await dbService.getCollection('user');
+        var users = await collection.find(criteria).toArray();
+        users = users.map((user) => {
+            delete user.password;
+
+            return user.type === 'doctor';
+        });
+        return users;
     } catch (err) {
-      logger.error('cannot find users', err)
-      throw err
+        logger.error('cannot find users', err);
+        throw err;
     }
 }
 
+async function getPatients(filterBy = {}) {
+    const criteria = _buildCriteria(filterBy);
+    try {
+        const collection = await dbService.getCollection('user');
+        var users = await collection.find(criteria).toArray();
+        users = users.map((user) => {
+            delete user.password;
 
+            return user.type === 'doctor';
+        });
+        return users;
+    } catch (err) {
+        logger.error('cannot find users', err);
+        throw err;
+    }
+}
 
 async function remove(userId) {
-  try {
-    const collection = await dbService.getCollection('user')
-    await collection.deleteOne({ _id: ObjectId(userId) })
-  } catch (err) {
-    logger.error(`cannot remove user ${userId}`, err)
-    throw err
-  }
+    try {
+        const collection = await dbService.getCollection('user');
+        await collection.deleteOne({ _id: ObjectId(userId) });
+    } catch (err) {
+        logger.error(`cannot remove user ${userId}`, err);
+        throw err;
+    }
 }
 
 async function update(user) {
-  try {
-    // peek only updatable fields!
-    const userToSave = {
-        ...user,
-      _id: ObjectId(user._id), // needed for the returnd obj
+    try {
+        // peek only updatable fields!
+        const userToSave = {
+            ...user,
+            _id: ObjectId(user._id), // needed for the returnd obj
+        };
+        const collection = await dbService.getCollection('user');
+        await collection.updateOne(
+            { _id: userToSave._id },
+            { $set: userToSave }
+        );
+        return userToSave;
+    } catch (err) {
+        logger.error(`cannot update user ${user._id}`, err);
+        throw err;
     }
-    const collection = await dbService.getCollection('user')
-    await collection.updateOne({ _id: userToSave._id }, { $set: userToSave })
-    return userToSave
-  } catch (err) {
-    logger.error(`cannot update user ${user._id}`, err)
-    throw err
-  }
 }
 
 async function add(user) {
-  try {
-    // peek only updatable fields!
-    const userToAdd = {
-      ...user,
-      username: user.username,
-      password: user.password,
-      UID: user.UID,
-      fullname: user.fullname,
-      isAdmin: false,
-      imgUrl: user.imgUrl,
+    try {
+        // peek only updatable fields!
+        const userToAdd = {
+            ...user,
+            username: user.username,
+            password: user.password,
+            UID: user.UID,
+            fullname: user.fullname,
+            isAdmin: false,
+            imgUrl: user.imgUrl,
+        };
+        if (user.type === 'patient') {
+            userToAdd.appointments = [];
+            userToAdd.inbox = [];
+        } else if (user.type === 'doctor') {
+            userToAdd.meetings = [];
+            userToAdd.patients = [];
+        }
+        const collection = await dbService.getCollection('user');
+        await collection.insertOne(userToAdd);
+        return userToAdd;
+    } catch (err) {
+        logger.error('cannot insert user', err);
+        throw err;
     }
-    if (user.type === 'patient') {
-      userToAdd.appointments = []
-      userToAdd.inbox = []
-    } else if (user.type === 'doctor') {
-      userToAdd.meetings = []
-      userToAdd.inbox = []
-    }
-    const collection = await dbService.getCollection('user')
-    await collection.insertOne(userToAdd)
-    return userToAdd
-  } catch (err) {
-    logger.error('cannot insert user', err)
-    throw err
-  }
 }
 
 function _buildCriteria(filterBy) {
-  let criteria = {}
-  if (filterBy.txt) {
-    const txtCriteria = { $regex: filterBy.txt, $options: 'i' }
-    criteria.$or = [
-      {
-        fullname: txtCriteria,
-      },
-    ]
-  }
-  if(filterBy.type){
-    //   const typeCriteria = filterBy.type
-    //   criteria.type = {"type":typeCriteria}
-    criteria = {"type":filterBy.type}
-  }
-  return criteria
+    let criteria = {};
+    if (filterBy.txt) {
+        const txtCriteria = { $regex: filterBy.txt, $options: 'i' };
+        criteria.$or = [
+            {
+                fullname: txtCriteria,
+            },
+        ];
+    }
+    if (filterBy.type) {
+        //   const typeCriteria = filterBy.type
+        //   criteria.type = {"type":typeCriteria}
+        criteria = { type: filterBy.type };
+    }
+    return criteria;
 }
